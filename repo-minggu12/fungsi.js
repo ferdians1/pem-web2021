e7b355354f73147f07688713b2185247943a3371
ferdians1 <111202012494@mhs.dinus.ac.id>
2021-12-14 22:56:15 +0700

    Tugas Minggu 12

---
+++ repo-minggu12/fungsi.js
@@ -0,0 +1,33 @@
+var dataBarang = ["Buku Tulis", "Pensil", "Spidol"];
+
+function showBarang() {
+    var listBarang = document.getElementById("list-barang");
+    // clear list barang
+    listBarang.innerHTML = "";
+
+    // Cetak Semua Barang
+    for (let i = 0; i < dataBarang.length; i++) {
+        var btnEdit = "<a href ='#' onClick='editBarang(" + i + ")'>Edit</a> ";
+        var btnHapus = "<a href ='#' onClick='deleteBarang(" + i + ")'>Hapus</a>";
+
+        listBarang.innerHTML += "<li>" + dataBarang[i] + "[" + btnEdit + "  " + btnHapus + "] </li>";
+    }
+}
+
+function addBarang() {
+    var input = document.querySelector("input[name=barang]");
+    dataBarang.push(input.value);
+    showBarang();
+}
+
+function editBarang(id) {
+    var newBarang = prompt("Nama Baru", dataBarang[id]);
+    dataBarang[id] = newBarang;
+    showBarang();
+}
+
+function deleteBarang(id) {
+    dataBarang.splice(id, 1);
+    showBarang;
+}
+showBarang();
